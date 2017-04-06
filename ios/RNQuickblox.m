6211994e573a44b45833bedd3a3950c40face784
Anh Dao <frankensteinruo@gmail.com>
2017-04-07 01:27:06 +1000

    [Testing] Linking with Quickblox framework.

--- ios/RNQuickblox.m
+++ ios/RNQuickblox.m
@@ -1,5 +1,6 @@
 
 #import "RNQuickblox.h"
+@import Quickblox;
 
 @implementation RNQuickblox
 
@@ -8,6 +9,27 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(
+                  login: (NSInteger) userId
+                  password: (NSString*) password
+                  resolver:(RCTPromiseResolveBlock) resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+{
+    QBUUser *user = [QBUUser user];
+    user.ID = (NSUInteger)userId;
+    user.password = password;
+    
+    [[QBChat instance] connectWithUser:user completion:^(NSError * _Nullable error) {
+        if (error) {
+            NSString *code = [NSString stringWithFormat:@"%ld", error.code];
+            reject(code, error.description, error);
+        } else {
+            resolve(user);
+        }
+    }];
+}
 
 @end
   
