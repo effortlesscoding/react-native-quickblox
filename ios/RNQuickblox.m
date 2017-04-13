f6fb4f8949bdb186455a3a61f5ae0fd2dd0a317f
Anh Dao <frankensteinruo@gmail.com>
2017-04-13 19:34:22 +1000

    Added a bunch of new methods and an event emitter.

--- ios/RNQuickblox.m
+++ ios/RNQuickblox.m
@@ -1,10 +1,12 @@
 
+#import <React/RCTEventDispatcher.h>
 #import "RNQuickblox.h"
 #import "QBError+ErrorsFormatting.h"
 #import "NSError+Initializers.h"
 #import "QBChatMessage+Formatting.h"
 @import UserNotifications;
 
+NSString *const QBDialogsLoaded = @"QBDialogsLoaded";
 NSString *const QBChatMessageSent = @"QBChatMessageSent";
 NSString *const QBChatMessageReceived = @"QBChatMessageReceived";
 
@@ -18,6 +20,8 @@ NSString *const QBChatMessageReceived = @"QBChatMessageReceived";
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+@synthesize bridge = _bridge;
 
 RCT_EXPORT_METHOD(
                   initialize:(RCTPromiseResolveBlock) resolve
@@ -61,7 +65,6 @@ RCT_EXPORT_METHOD(
     }];
 }
 
-
 RCT_EXPORT_METHOD(
                   loadDialogs: (NSString *) userId
                   resolver:(RCTPromiseResolveBlock) resolve
@@ -71,9 +74,10 @@ RCT_EXPORT_METHOD(
     [QBRequest dialogsWithSuccessBlock:^(QBResponse * _Nonnull response, NSArray<QBChatDialog *> * _Nullable dialogObjects, NSSet<NSNumber *> * _Nullable dialogsUsersIDs) {
         NSArray *mappings = [self dictionaryMappingsFrom: dialogObjects withUser:userId];
         dialogs = mappings[0];
-        resolve(@{
-                  @"usersDialogs": mappings[1]
-                  });
+        NSDictionary *data = @{
+                               @"usersDialogs": mappings[1]
+                               };
+        resolve(data);
     } errorBlock:^(QBResponse * _Nonnull response) {
         if (response.error) {
             reject(
@@ -93,6 +97,39 @@ RCT_EXPORT_METHOD(
     }];
 }
 
+RCT_EXPORT_METHOD(
+                  createPrivateDialog: (NSNumber *) myQuickbloxId
+                  opponent:(NSNumber *) opponentQuickbloxId
+                  resolver:(RCTPromiseResolveBlock) resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+{
+    QBChatDialog *chatDialog = [[QBChatDialog alloc] initWithDialogID:nil type:QBChatDialogTypePrivate];
+    chatDialog.name = @"Private chat";
+    chatDialog.occupantIDs = @[myQuickbloxId, opponentQuickbloxId];
+    [QBRequest createDialog:chatDialog successBlock:^(QBResponse * _Nonnull response, QBChatDialog * _Nullable createdDialog) {
+        NSDictionary *data = @{
+                               @"dialogId": createdDialog.ID
+                               };
+        resolve(data);
+    } errorBlock:^(QBResponse * _Nonnull response) {
+        if (response.error) {
+            reject(
+                   [response.error errorCode],
+                   [response.error errorsSentence],
+                   response.error.error
+                   );
+        } else {
+            NSInteger code = 99;
+            NSString *reason = @"Quickblox response does not have an error";
+            reject(
+                   [NSString stringWithFormat:@"%ld", code],
+                   reason,
+                   [NSError errorWithCode:code reason:reason]
+                   );
+        }
+    }];
+}
 
 RCT_EXPORT_METHOD(
                   sendTextChat:(NSString*)text
@@ -106,7 +143,43 @@ RCT_EXPORT_METHOD(
         return;
     }
     QBChatMessage *message = [QBChatMessage message];
+    NSMutableDictionary *customParams = [NSMutableDictionary new];
+    customParams[@"save_to_history"] = @"1";
+    message.customParameters = customParams;
     [message setText:text];
+    // TODO: Set a couple other parameters ...
+    // TODO: Loop through all dialogs that you found ...
+    [dialog sendMessage:message completionBlock:^(NSError * _Nullable error) {
+        if (error) {
+            NSString *code = [NSString stringWithFormat:@"%ld", error.code];
+            reject(code, error.description, error);
+        } else {
+            resolve(@"Successfuly sent");
+        }
+    }];
+}
+
+RCT_EXPORT_METHOD(
+                  sendVideoChat:(NSString*)videoUrl
+                  thumbnail:(NSString*)thumbnailUrl
+                  text:(NSString*)additionalMessage
+                  dialogId: (NSString*)dialogId
+                  resolver:(RCTPromiseResolveBlock) resolve
+                  rejecter:(RCTPromiseRejectBlock)reject
+                  )
+{
+    QBChatDialog *dialog = [dialogs objectForKey:dialogId];
+    if (!dialog) {
+        return;
+    }
+    QBChatMessage *message = [QBChatMessage message];
+    NSMutableDictionary *customParams = [NSMutableDictionary new];
+    customParams[@"save_to_history"] = @"1";
+    customParams[@"message_type"] = @"v";
+    customParams[@"thumbnail_url"] = thumbnailUrl;
+    customParams[@"video_url"] = videoUrl;
+    message.customParameters = customParams;
+    [message setText:additionalMessage];
     // TODO: Set a couple other parameters ...
     // TODO: Loop through all dialogs that you found ...
     [dialog sendMessage:message completionBlock:^(NSError * _Nullable error) {
@@ -123,7 +196,7 @@ RCT_EXPORT_METHOD(
 {
     // TODO: Handle it more properly
     NSInteger userId = [userIdParam integerValue];
-    NSMutableDictionary<NSNumber *, NSString*>  *usersDialogIdsMappings = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary<NSString *, NSString*>  *usersDialogIdsMappings = [[NSMutableDictionary alloc] init];
     NSMutableDictionary<NSString *, QBChatDialog *> *dialogIdMapping = [[NSMutableDictionary alloc] init];
     for (QBChatDialog *dialog in dialogObjects) {
         NSNumber *opponentId = [NSNumber numberWithInteger:-1];
@@ -134,7 +207,7 @@ RCT_EXPORT_METHOD(
         }
         if (opponentId.integerValue > -1) {
             [dialogIdMapping setObject: dialog forKey: dialog.ID];
-            [usersDialogIdsMappings setObject:dialog.ID forKey:opponentId];
+            [usersDialogIdsMappings setObject: dialog.ID forKey:[opponentId stringValue]];
         }
     }
     return @[
@@ -143,7 +216,19 @@ RCT_EXPORT_METHOD(
              ];
 }
 
+#pragma mark Quickblox Notifications setup
+
+- (void)setBridge:(RCTBridge *)bridge
+{
+    _bridge = bridge;
+}
+
 #pragma mark QBChatDelegate
+
+- (void) dialogsLoaded
+{
+    
+}
 
 /**
  *  Called whenever message was delivered to user.
@@ -154,15 +239,12 @@ RCT_EXPORT_METHOD(
  */
 - (void)chatDidDeliverMessageWithID:(NSString *)messageID dialogID:(NSString *)dialogID toUserID:(NSUInteger)userID
 {
-    [[NSNotificationCenter defaultCenter]
-     postNotificationName:QBChatMessageReceived
-     object:self
-     userInfo: @{
-                 @"messageID": messageID,
-                 @"dialogID": dialogID,
-                 @"userID": [NSNumber numberWithInteger: userID]
-                 }
-     ];
+    NSDictionary *params = @{
+                            @"messageID": messageID,
+                            @"dialogID": dialogID,
+                            @"userID": [NSNumber numberWithInteger: userID]
+                            };
+    [_bridge.eventDispatcher sendDeviceEventWithName:QBChatMessageSent body:params];
 }
 
 /**
@@ -174,11 +256,7 @@ RCT_EXPORT_METHOD(
  */
 - (void)chatDidReceiveMessage:(QBChatMessage *)message
 {
-    [[NSNotificationCenter defaultCenter]
-        postNotificationName:QBChatMessageReceived
-        object:self
-        userInfo: [message dictionaryValue]
-     ];
+    [_bridge.eventDispatcher sendDeviceEventWithName:QBChatMessageReceived body:[message dictionaryValue]];
 }
 @end
 
